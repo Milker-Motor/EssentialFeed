c8ef87658d2005383a2fc7ef10e0aed486b569e4
Oleksii Lytvynov-Bohdanov <lytvynov-bohdanov@curamatik.de>
2023-08-15 23:55:32 +0300

    Remove unused RemoteLoader

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -43,21 +43,27 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 	
 	// MARK: - Helpers
 	
-	private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
-        let loader = RemoteLoader(url: feedTestServerURL, client: ephemeralClient(), mapper: FeedItemsMapper.map)
-		trackForMemoryLeaks(loader, file: file, line: line)
-		
-		let exp = expectation(description: "Wait for load completion")
-		
-		var receivedResult: FeedLoader.Result?
-		loader.load { result in
-			receivedResult = result
-			exp.fulfill()
-		}
-		wait(for: [exp], timeout: 5.0)
-		
-		return receivedResult
-	}
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
+        let client = ephemeralClient()
+        trackForMemoryLeaks(loader, file: file, line: line)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: FeedLoader.Result?
+        client.get(from: feedTestServerURL) { result in
+            receivedResult = result.flatMap { (data, response) in
+                do {
+                    return .success(try FeedItemsMapper.map(data, from: response))
+                } catch {
+                    return .failure(error)
+                }
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        
+        return receivedResult
+    }
 	
 	private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
 		let loader = RemoteFeedImageDataLoader(client: ephemeralClient())
