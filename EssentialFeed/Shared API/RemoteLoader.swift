bccdf969999b2c4096c680a9298d0b05cae04c28
Oleksii Lytvynov-Bohdanov <lytvynov-bohdanov@curamatik.de>
2023-08-15 16:06:08 +0300

    Duplicate RemoteFeedLoader as RemoteLoader

---
+++ EssentialFeed/Shared API/RemoteLoader.swift
@@ -0,0 +1,9 @@
+//
+//  RemoteLoader.swift
+//  EssentialFeed
+//
+//  Created by Oleksii Lytvynov-Bohdanov on 15.08.2023.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import Foundation
