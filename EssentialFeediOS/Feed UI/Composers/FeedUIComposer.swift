7a3a809e7e1b79533e413b0c091cbb22a0102e62
Oleksii Lytvynov-Bohdanov <lytvynov-bohdanov@curamatik.de>
2023-08-22 13:15:33 +0300

    Replace FeedViewControllerDelegate with a closure

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -13,10 +13,9 @@ public final class FeedUIComposer {
     public static func feedComposedWith(feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> ListViewController {
         let presentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>(loader: feedLoader().dispatchOnMainQueue)
 		
-		let feedController = makeFeedViewController(
-			delegate: presentationAdapter,
-			title: FeedPresenter.title)
-
+		let feedController = makeFeedViewController(title: FeedPresenter.title)
+        feedController.onRefresh = presentationAdapter.loadResource
+        
 		presentationAdapter.presenter = LoadResourcePresenter(
 			resourceView: FeedViewAdapter(
 				controller: feedController,
@@ -29,11 +28,11 @@ public final class FeedUIComposer {
 		return feedController
 	}
 
-	private static func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> ListViewController {
+	private static func makeFeedViewController(title: String) -> ListViewController {
 		let bundle = Bundle(for: ListViewController.self)
 		let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
 		let feedController = storyboard.instantiateInitialViewController() as! ListViewController
-		feedController.delegate = delegate
+		
 		feedController.title = title
 		return feedController
 	}
