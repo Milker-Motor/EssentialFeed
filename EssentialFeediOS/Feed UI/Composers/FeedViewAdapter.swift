1da8ac8a078a282cedaf4d53bf963f13cf855ed3
Oleksii Lytvynov-Bohdanov <lytvynov-bohdanov@curamatik.de>
2023-08-28 13:09:26 +0300

    Use diffable data source to only reload cells when needed

--- EssentialFeediOS/Feed UI/Composers/FeedViewAdapter.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedViewAdapter.swift
@@ -34,7 +34,7 @@ final class FeedViewAdapter: ResourceView {
                     }
                     return image
                 })
-            return CellController(view)
+            return CellController(id: model, view)
         })
     }
 }
