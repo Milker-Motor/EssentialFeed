4b084e36d9cff1f4934e799b339dfe7f4835dd7d
Oleksii Lytvynov-Bohdanov <lytvynov-bohdanov@curamatik.de>
2023-09-01 12:08:17 +0300

    Remove feed references from comments integration tests

--- EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -40,3 +40,19 @@ extension Date {
         calendar.date(byAdding: .day, value: days, to: self)!
     }
 }
+
+private class DummyView: ResourceView {
+    func display(_ viewModel: Any) {}
+}
+
+var loadError: String {
+    LoadResourcePresenter<Any, DummyView>.loadError
+}
+
+var feedTitle: String {
+    FeedPresenter.title
+}
+
+var commentsTitle: String {
+    ImageCommentsPresenter.title
+}
