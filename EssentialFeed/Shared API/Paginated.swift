75c595c69da89301f0b98d3ad2a291808353ddf0
Oleksii Lytvynov-Bohdanov <lytvynov-bohdanov@curamatik.de>
2023-09-25 11:13:04 +0300

    Make feed loader closure return Paginated<FeedImage>

---
+++ EssentialFeed/Shared API/Paginated.swift
@@ -0,0 +1,9 @@
+//
+//  Paginated.swift
+//  EssentialFeed
+//
+//  Created by Oleksii Lytvynov-Bohdanov on 25.09.2023.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import Foundation
