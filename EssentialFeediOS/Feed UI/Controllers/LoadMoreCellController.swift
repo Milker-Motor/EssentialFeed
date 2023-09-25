966d92a2540354af1b3c2d7b4fbe02967d830bf3
Oleksii Lytvynov-Bohdanov <lytvynov-bohdanov@curamatik.de>
2023-09-25 10:27:21 +0300

    Add "Load more" cell controller with loading indicator

---
+++ EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -0,0 +1,9 @@
+//
+//  LoadMoreCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Oleksii Lytvynov-Bohdanov on 25.09.2023.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import Foundation
