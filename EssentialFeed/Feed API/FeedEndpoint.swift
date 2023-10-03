8886cff93e40f1a65e399f87cf53b8844a94fc81
Oleksii Lytvynov-Bohdanov <lytvynov-bohdanov@curamatik.de>
2023-10-03 17:32:35 +0300

    Set FeedEndpoint limit to 10

---
+++ EssentialFeed/Feed API/FeedEndpoint.swift
@@ -0,0 +1,9 @@
+//
+//  FeedEndpoint.swift
+//  EssentialFeed
+//
+//  Created by Oleksii Lytvynov-Bohdanov on 03.10.2023.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import Foundation
---
+++ EssentialFeedTests/Feed API/FeedEndpointTests.swift
@@ -0,0 +1,9 @@
+//
+//  FeedEndpointTests.swift
+//  EssentialFeedTests
+//
+//  Created by Oleksii Lytvynov-Bohdanov on 03.10.2023.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import Foundation
