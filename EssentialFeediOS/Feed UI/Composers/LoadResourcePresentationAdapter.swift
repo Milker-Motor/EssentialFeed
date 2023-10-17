f53e26f1f9da5abee1e4a1a8e8fb90fbd3ede957
Oleksii Lytvynov-Bohdanov <lytvynov-bohdanov@curamatik.de>
2023-10-17 09:50:30 +0300

    Prevent loading resources again until a running request completes

--- EssentialFeediOS/Feed UI/Composers/LoadResourcePresentationAdapter.swift
+++ EssentialFeediOS/Feed UI/Composers/LoadResourcePresentationAdapter.swift
@@ -9,6 +9,7 @@ import EssentialFeediOS
 final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     private let loader: () -> AnyPublisher<Resource, Error>
     private var cancellable: Cancellable?
+    private var isLoading = false
     var presenter: LoadResourcePresenter<Resource, View>?
     
     init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
@@ -16,18 +17,27 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     }
     
     func loadResource() {
-        presenter?.didStartLoading()
+        guard !isLoading else { return }
         
-        cancellable = loader().sink(receiveCompletion: { [weak self] completion in
-            switch completion {
-            case .finished: break
-                
-            case let .failure(error):
-                self?.presenter?.didFinishLoading(with: error)
+        presenter?.didStartLoading()
+        isLoading = true
+        
+        cancellable = loader()
+            .dispatchOnMainQueue()
+            .handleEvents(receiveCancel: { [weak self] in
+                self?.isLoading = false
+            })
+            .sink(receiveCompletion: { [weak self] completion in
+                switch completion {
+                case .finished: break
+                    
+                case let .failure(error):
+                    self?.presenter?.didFinishLoading(with: error)
+                }
+                self?.isLoading = false
+            }) { [weak self] resource in
+                self?.presenter?.didFinishLoading(with: resource)
             }
-        }) { [weak self] resource in
-            self?.presenter?.didFinishLoading(with: resource)
-        }
     }
 }
 
