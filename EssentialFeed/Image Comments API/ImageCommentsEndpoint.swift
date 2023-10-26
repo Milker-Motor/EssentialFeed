b9304fbb7b1d5c37d8ee0a99585078d3e15d249e
Oleksii Lytvynov-Bohdanov <lytvynov-bohdanov@curamatik.de>
2023-10-26 12:53:36 +0300

    Do request for image on next present

---
+++ EssentialFeed/Image Comments API/ImageCommentsEndpoint.swift
@@ -0,0 +1,9 @@
+//
+//  ImageCommentsEndpoint.swift
+//  EssentialFeed
+//
+//  Created by Oleksii Lytvynov-Bohdanov on 25.10.2023.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import Foundation
